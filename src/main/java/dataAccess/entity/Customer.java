65829203e8303646e5778a607b950089f0a236ae
Shima Abbasi <shima.abbasi1992@gmail.com>
2016-08-21 16:50:39 +0430

    commit:D

--- src/main/java/dataAccess/entity/Customer.java
+++ src/main/java/dataAccess/entity/Customer.java
@@ -1,7 +1,30 @@
 package dataAccess.entity;
 
+import javax.persistence.*;
+
 /**
  * Created by Dotin school 5 on 8/21/2016.
  */
+
+@Entity
+@Table(name = "CUSTOMER")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Customer {
+    @Id
+    @GeneratedValue
+    @Column(name = "CUSTOMER_ID", nullable = false, insertable = false, updatable = false, unique = true)
+    private int customerId;
+
+    public Customer() {
+
+    }
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+
 }
--- src/main/java/dataAccess/entity/RealCustomer.java
+++ src/main/java/dataAccess/entity/RealCustomer.java
@@ -1,7 +1,43 @@
 package dataAccess.entity;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Dotin school 5 on 8/21/2016.
  */
-public class RealCustomer {
+
+@Entity
+@Table(name = "REAL_CUSTOMER")
+@PrimaryKeyJoinColumn(name = "CUSTOMER_ID")
+public class RealCustomer extends Customer {
+
+    @Column(name = "FIRST_NAME", nullable = false)
+    private String firstName;
+
+    @Column(name = "LAST_NAME", nullable = false)
+    private String lastName;
+
+    @Column(name = "FATHER_NAME", nullable = false)
+    private String fatherName;
+
+    @Column(name = "DATE_OF_BIRTH", nullable = false)
+    private String dateOfBirth;
+
+    @Column(name = "INTERNATIONAL_ID", nullable = false)
+    private String internationalID;
+
+    @OneToMany(mappedBy = "realCustomer" , cascade = {CascadeType.ALL  })
+    private Set<LoanFile> loanFiles = new HashSet<LoanFile>(0);
+
+    public RealCustomer(String firstName, String lastName, String fatherName, String dateOfBirth, String internationalID, Set<LoanFile> loanFiles) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fatherName = fatherName;
+        this.dateOfBirth = dateOfBirth;
+        this.internationalID = internationalID;
+        this.loanFiles = loanFiles;
+    }
 }
